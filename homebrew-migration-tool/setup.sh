638abe14c30a31ab6692258618404d64b2bed2b4
HaraldNordgren <haraldnordgren@gmail.com>
2016-11-21 00:29:18 +0100

    Moving files back to project root

--- homebrew-migration-tool/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git remote add homebrew-versions-origin https://github.com/Homebrew/homebrew-versions
-git fetch homebrew-versions-origin
-git branch homebrew-versions -u homebrew-versions-origin/master
-
