2cdc353553030342518b58b024e8f6104ea21f44
HaraldNordgren <haraldnordgren@gmail.com>
2016-11-22 00:35:09 +0100

    Clone fresh repo on every deploy

--- run.sh
+++ run.sh
@@ -5,34 +5,25 @@ set -e
 mkdir -p .ssh
 echo "$GITHUB_PRIVATE_SSH_KEY" > .ssh/id_rsa
 chmod 600 .ssh/id_rsa
-#ssh -i .ssh/id_rsa -oStrictHostKeyChecking=no -T git@github.com
-#ssh-add .ssh/id_rsa < /dev/null
+ssh-keyscan github.com >> .ssh/known_hosts
 
-if [ -d homebrew-versions ]; then
-    cd homebrew-versions
-    git reset --hard -q
-    git clean -fd
-    git pull
-else
-    #git clone https://github.com/HaraldNordgren/homebrew-versions
-    git clone git@github.com:HaraldNordgren/homebrew-versions.git
-    cd homebrew-versions
-    git remote add homebrew-versions-origin https://github.com/Homebrew/homebrew-versions
-    git fetch homebrew-versions-origin
-    git checkout -b homebrew-versions homebrew-versions-origin/master
-fi
+rm -rf homebrew-versions homebrew-migration-tool
+
+git clone git@github.com:HaraldNordgren/homebrew-versions.git
+cd homebrew-versions
+git remote add homebrew-versions-origin https://github.com/Homebrew/homebrew-versions
+git fetch homebrew-versions-origin
+git checkout -b homebrew-versions homebrew-versions-origin/master
+
+git config user.email "haraldnordgren+homebrew-version-migration-bot@gmail.com"
+git config user.name "homebrew-version-migration-bot"
+git config push.default simple
 
 cd -
 echo
 
-if [ -d homebrew-migration-tool ]; then
-    cd homebrew-migration-tool
-    git pull
-    cd -
-else
-    git clone https://github.com/HaraldNordgren/homebrew-migration-tool
-fi
-
+git clone https://github.com/HaraldNordgren/homebrew-migration-tool
 echo
 
 homebrew-migration-tool/migrate.sh homebrew-versions
+
