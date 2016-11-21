679448b2e2302128f51b52d82bb2fdb4a2f42460
Harald Nordgren <harald@cantemo.com>
2016-11-21 23:45:54 +0100

    Use ssh for homebrew-versions

--- run.sh
+++ run.sh
@@ -2,13 +2,20 @@
 
 set -e
 
+mkdir -p .ssh
+echo "$GITHUB_PRIVATE_SSH_KEY" > .ssh/id_rsa
+chmod 600 .ssh/id_rsa
+#ssh -i .ssh/id_rsa -oStrictHostKeyChecking=no -T git@github.com
+#ssh-add .ssh/id_rsa < /dev/null
+
 if [ -d homebrew-versions ]; then
     cd homebrew-versions
     git reset --hard -q
     git clean -fd
     git pull
 else
-    git clone https://github.com/HaraldNordgren/homebrew-versions
+    #git clone https://github.com/HaraldNordgren/homebrew-versions
+    git clone git@github.com:HaraldNordgren/homebrew-versions.git
     cd homebrew-versions
     git remote add homebrew-versions-origin https://github.com/Homebrew/homebrew-versions
     git fetch homebrew-versions-origin
