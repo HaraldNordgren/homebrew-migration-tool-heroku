b258d8bec037c1fa3a8d0303cd2301343de9e611
HaraldNordgren <haraldnordgren@gmail.com>
2016-11-23 00:58:01 +0100

    Consolidated migrate.sh and run.sh scripts and refactored

--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@ echo "$GITHUB_PRIVATE_SSH_KEY" > .ssh/id_rsa
 chmod 600 .ssh/id_rsa
 ssh-keyscan github.com >> .ssh/known_hosts
 
-rm -rf homebrew-versions
 git clone git@github.com:HaraldNordgren/homebrew-versions.git
 cd homebrew-versions
 
@@ -21,8 +20,36 @@ git config user.email "haraldnordgren+homebrew-version-migration-bot@gmail.com"
 git config user.name "homebrew-version-migration-bot"
 git config push.default simple
 
-cd -
 echo
+echo "PULLING LATEST COMMITS FROM HOMEBREW-VERSIONS"
+git pull
 
-./migrate.sh homebrew-versions
+latest_homebrew_commit=$(git rev-parse HEAD)
+if git log master --pretty=%B | grep -q $latest_homebrew_commit ; then
+    echo "NOTHING NEW TO MIGRATE"
+    exit 0
+fi
+
+echo
+staging_branch=homebrew-versions-$latest_homebrew_commit
+git checkout -b $staging_branch
+
+ruby migrate_versions.rb
+
+git add . -A
+git commit -m "Migrated 'Homebrew/homebrew-versions' up to $latest_homebrew_commit" -q
+
+echo "MERGING BRANCHES"
+git checkout master -q
+git merge $staging_branch -X theirs --no-edit -q
+
+echo
+echo "PUSHING TO REMOTE"
+git push
+git branch -d $staging_branch
+
+git checkout homebrew-versions -q
+
+echo "MIGRATION COMPLETED"
+exit 0
 
