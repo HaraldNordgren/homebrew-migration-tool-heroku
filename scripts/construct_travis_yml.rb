c89c958af64340d6d52ab1638a91c2279ef53357
HaraldNordgren <haraldnordgren@gmail.com>
2016-12-27 15:46:57 +0100

    Moved logic from build_formula to travis yml file

--- scripts/construct_travis_yml.rb
+++ scripts/construct_travis_yml.rb
@@ -1,25 +1,73 @@
 require 'yaml'
 
 
+def package_specific_before_build_commands(file_without_extension)
+    cmd_list = []
+
+    if file_without_extension =~ /gdal[@]?111/
+        cmd_list.push("brew unlink gdal")
+    elsif file_without_extension =~ /gnupg[@]?21/
+        cmd_list.push("brew unlink gnupg2 gpg-agent dirmngr")
+    elsif file_without_extension =~ /go[@]?15/
+        cmd_list.push("brew unlink go")
+    elsif file_without_extension =~ /jpeg[@]?gb|jpeg[@]?6b/
+        cmd_list.push("brew unlink jpeg")
+    elsif file_without_extension =~ /ruby[@]?192/
+        cmd_list.push("brew unlink ruby")
+    elsif file_without_extension =~ /subversion[@]?18/
+        cmd_list.push("brew unlink subversion")
+    elsif file_without_extension =~ /povray[@]?36/
+        cmd_list.push("brew unlink libpng")
+    elsif file_without_extension =~ /postgresql[@]?[0-9]+/
+        cmd_list.push("brew unlink postgresql")
+    elsif file_without_extension =~ /lz4[@]?r117/
+        cmd_list.push("brew unlink lz4")
+    elsif file_without_extension =~ /appledoc/
+        cmd_list.push("brew install gcc")
+    elsif file_without_extension =~ /duplicity/
+        cmd_list.push("pip install testrepository")
+    elsif file_without_extension =~ /gcc[@]?6/
+        cmd_list.push("rm /usr/local/include/c++")
+    end
+
+    if file_without_extension =~ /automake/
+        cmd_list.push("brew unlink automake")
+    elsif file_without_extension !~ /influxdb[@]?08|ledger[@]?26|llvm[@]?35|thrift[@]?090|riak[@]?132|rebar[@]?3|rabbitmq[@]?30|erlang|gdal[@]?111/
+        cmd_list.push("brew unlink autoconf")
+        cmd_list.push("brew unlink automake")
+    end
+
+    return cmd_list
+end
+
 repo_name = ARGV[0]
 output_file = ARGV[1]
-
-formulas = []
+tap_short_name = 'versions'
 
 skip_builds = {
     "xcode8.2" => [
         "allegro@4",
         "appledoc@20",
         "appledoc@21",
+        "gcc@43",
+        "gcc@44",
+        "gcc@45",
+        "gcc@46", # Times out, check for problems on migrated!
+        "gcc@47", # Times out, check for problems on migrated!
+        "gcc@48", # Times out, check for problems on migrated!
+        "gcc@49", # Times out, check for problems on migrated!
+        "gcc@5", # Times out, check for problems on migrated!
+        "go@16",
     ],
     "xcode8.1" => [],
     "xcode8" => [],
     "xcode7.3" => [],
     "xcode6.4" => [],
 }
+formulas = []
 
 if repo_name == 'reference'
-    # skip_packages_string = ARGV[0].gsub("-@", "-").gsub("@", "")
+    tap_author = 'homebrew'
     skip_builds.each do |xcode, skip_list|
         new_list = []
         for package in skip_list
@@ -34,7 +82,7 @@ if repo_name == 'reference'
     end
 
 elsif repo_name == 'versions'
-    # skip_packages_string = ARGV[0].gsub("-@", "@")
+    tap_author = 'haraldnordgren'
     skip_builds.each do |xcode, skip_list|
         new_list = []
         for package in skip_list
@@ -55,56 +103,53 @@ end
 
 formulas.sort!
 
-includes = []
-
+before_script_prefix = [
+    # "brew unlink automake",
+    # "brew unlink autoconf",
+    "brew unlink maven",
+]
 before_script = {
-    "xcode8.2" => [
-        "brew unlink automake",
-        "brew unlink autoconf",
-        "brew unlink maven",
-    ],
-    "xcode8.1" => [
-        "brew unlink automake",
-        "brew unlink autoconf",
-        "brew unlink maven",
-    ],
+    "xcode8.2" => [],
+    "xcode8.1" => [],
     "xcode8" => [
-        "brew unlink automake",
-        "brew unlink autoconf",
-        "brew unlink maven",
         "brew untap homebrew/versions",
     ],
     "xcode7.3" => [
-        "brew unlink automake",
-        "brew unlink autoconf",
-        "brew unlink maven",
         "brew untap homebrew/versions",
     ],
     "xcode6.4" => [
-        "brew unlink automake",
-        "brew unlink autoconf",
-        "brew unlink maven",
         "brew untap homebrew/versions",
     ],
 }
+before_script_suffix = [
+    "brew tap #{tap_author}/#{tap_short_name}",
+    "brew update",
+    "brew update",
+    "brew doctor",
+]
 
-for xcode in ["xcode8.2", "xcode8.1", "xcode8", "xcode7.3", "xcode6.4"]
-# for xcode in ["xcode8.2"]
+includes = []
+#for xcode in ["xcode8.2", "xcode8.1", "xcode8", "xcode7.3", "xcode6.4"]
+for xcode in ["xcode8"]
     skip_build = skip_builds[xcode]
-    before = before_script[xcode]
+    xcode_before = before_script[xcode]
 
     for formula in formulas
         if skip_build.include?(formula)
             next
         end
 
+        unlink_before = package_specific_before_build_commands(formula)
+        before = unlink_before + before_script_prefix + xcode_before + before_script_suffix
+
+        package_full_name = "#{tap_author}/#{tap_short_name}/#{formula}"
         includes.push({
             "language" => "ruby",
-            "env" => "FORMULA=#{formula}",
+            "env" => "Formula=#{formula}",
             "os" => "osx",
             "osx_image" => xcode,
             "before_script" => before,
-            "script" => 'travis_wait 50 ruby tests/build_formula.rb $FORMULA',
+            "script" => "travis_wait 50 ruby tests/build_formula.rb #{package_full_name}",
         })
     end
 end
@@ -128,6 +173,6 @@ output_yml = {
 #puts
 
 File.open(output_file,"w") do |file|
-   file.write "# This file is auto-generated by #{__FILE__}"
+   file.write "# This file is auto-generated by #{File.basename(__FILE__)}\n"
    file.write output_yml.to_yaml
 end
