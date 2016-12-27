48821b923b3b4b0b5b559aab08c151cf19e6a1e1
HaraldNordgren <haraldnordgren@gmail.com>
2016-12-27 02:14:19 +0100

    Handle unlinkings in new build matrix

--- scripts/construct_travis_yml.rb
+++ scripts/construct_travis_yml.rb
@@ -6,23 +6,28 @@ output_file = ARGV[1]
 
 formulas = []
 
+XCODES = ["xcode8.2", "xcode8.1", "xcode8", "xcode7.3", "xcode6.4"]
+
 skip_builds = {}
-for xcode in ["xcode8.2", "xcode8.1", "xcode8", "xcode7.3", "xcode6.4"]
+for xcode in XCODES
     skip_builds[xcode] = []
 end
 
-skip_builds["xcode8.2"] = ["allegro@4"]
+skip_builds["xcode8.2"] = [
+    "allegro@4",
+    "appledoc@20",
+    "appledoc@21",
+]
 
 if repo_name == 'reference'
     # skip_packages_string = ARGV[0].gsub("-@", "-").gsub("@", "")
+
     puts Dir.pwd
     for file_name in Dir["*.rb"]
         puts file_name
         formula = File.basename(file_name, File.extname(file_name))
         formulas.push(formula)
     end
-
-    #    for file_name in Dir["*.rb"]; puts File.basename(file_name, File.extname(file_name)); end
 
 elsif repo_name == 'versions'
     # skip_packages_string = ARGV[0].gsub("-@", "@")
@@ -40,10 +45,41 @@ formulas.sort!
 
 includes = []
 
+before_script = {
+    "xcode8.2" => [
+        "brew unlink automake",
+        "brew unlink autoconf",
+        "brew unlink maven",
+    ],
+    "xcode8.1" => [
+        "brew unlink automake",
+        "brew unlink autoconf",
+        "brew unlink maven",
+    ],
+    "xcode8" => [
+        "brew unlink automake",
+        "brew unlink autoconf",
+        "brew unlink maven",
+        "brew untap homebrew/versions",
+    ],
+    "xcode7.3" => [
+        "brew unlink automake",
+        "brew unlink autoconf",
+        "brew unlink maven",
+        "brew untap homebrew/versions",
+    ],
+    "xcode6.4" => [
+        "brew unlink automake",
+        "brew unlink autoconf",
+        "brew unlink maven",
+        "brew untap homebrew/versions",
+    ],
+}
 
 # for xcode in ["xcode8.2", "xcode8.1", "xcode8", "xcode7.3", "xcode6.4"]
 for xcode in ["xcode8.2"]
     skip_build = skip_builds[xcode]
+    before = before_script[xcode]
 
     for formula in formulas
         if skip_build.include?(formula)
@@ -53,7 +89,8 @@ for xcode in ["xcode8.2"]
         includes.push({
             "env" => "FORMULA=#{formula}",
             "osx_image" => xcode,
-            "script" => 'ruby tests/build_formula.rb $FORMULA',
+            "before_script" => before,
+            "script" => 'travis_wait 50 ruby tests/build_formula.rb $FORMULA',
         })
     end
 end
